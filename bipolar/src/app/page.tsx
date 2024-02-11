3d0cec0ce372cbf181d8a3f577dbdefb984ce70f
host-ghost <77846509+host-ghost@users.noreply.github.com>
2024-02-11 19:41:47 +0530

    first commit

---
+++ bipolar/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Index from './component/Index'
+
+const page = () => {
+  return (
+    <div>
+        dineshs
+        <Index/>
+    </div>
+  )
+}
+
+export default page
